19c8e2d438ccfa81ecaba292b96353a4e8a1058f
[tre1000] <[tylerepstein27@gmail.com]>
2021-05-27 21:50:16 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Vue.js</h1>
+    <h3>{{ vueWatchers }}</h3>
+    <br>
+    <h1>Angular</h1>
+    <h3>{{ angularWatchers }}</h3>
+    <br>
+    <h1>Svelte</h1>
+    <h3>{{ svelteWatchers }}</h3>
+    <br>
+    <h1>React</h1>
+    <h3>{{ reactWatchers }}</h3>
+    <br>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      vueWatchers: "",
+      angularWatchers: "",
+      svelteWatchers: "",
+      reactWatchers: "",
+    }
   },
-};
+
+  created: function () {
+    this.getWatchers();
+  },
+
+  methods: {
+    getWatchers: function () {
+      //Vue.js
+      axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
+        this.vueWatchers = response.data.watchers_count;
+      });
+      //angular
+      axios.get("https://api.github.com/repos/angular/angular.js").then((response) => {
+        this.angularWatchers = response.data.watchers_count;
+      });
+      //svelte
+      axios.get("https://api.github.com/repos/sveltejs/svelte").then((response) => {
+        this.svelteWatchers = response.data.watchers_count;
+      });
+      //react
+      axios.get("https://api.github.com/repos/facebook/react").then((response) => {
+        this.reactWatchers = response.data.watchers_count;
+      });
+    },
+  }
+}
 </script>
